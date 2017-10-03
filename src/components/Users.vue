7ea4a0b572397c3a9a961b083f9a300df9a835b2
Edilson Borges <edilsonfb@gmail.com>
2017-10-02 23:02:31 -0300

    Users data and for to show values

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,19 +1,39 @@
 <template>
   <div class="users">
+      <h1>Users</h1>
+      <ul>
+        <li v-for="user in users">{{user.name}}: {{user.email}}</li>
+      </ul>
   </div>
 </template>
 
 <script>
-    export default {
-      name: 'users',
-      data () {
-        return {
-
-        }
-      },
-      methods: {
-
+  export default {
+    name: 'users',
+    data () {
+      return {
+        users: [
+          {
+            name: 'John Doe',
+            email: 'jdoe@gmail.com',
+            contacted: false
+          },
+          {
+            name: 'Steve smith',
+            email: 'ssmith@gmail.com',
+            contacted: false
+          },
+          {
+            name: 'Tom White',
+            email: 'tomwhite@gmail.com',
+            contacted: false
+          }
+        ]
       }
+    },
+    methods: {
+
+    }
 }
 </script>
 
