076d33dc8e48f90f41b004a0a4040c132376fa42
Edilson Borges <edilsonfb@gmail.com>
2017-10-02 22:49:31 -0300

    new compoent: users

---
+++ src/components/Users.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="users">
+  </div>
+</template>
+
+<script>
+    export default {
+      name: 'users',
+      data () {
+        return {
+
+        }
+      },
+      methods: {
+
+      }
+}
+</script>
+
+<style scoped>
+
+</style>
